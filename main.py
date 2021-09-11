acb8e9f6827ce93d9d92abe535bae18cd782d435
Dat <nguyenthanhdat070701@gmail.com>
2021-09-11 14:35:30 +0700

    finish

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pygame
-from tiktoktoe.constant import WIDTH, HEIGHT, COLUMN, ROW, SIZE, TAN, GREEN, BLACK, WHITE
+from pygame.constants import KSCAN_Z
+from tiktoktoe.constant import (
+    WIDTH,
+    HEIGHT,
+    COLUMN,
+    ROW,
+    SIZE,
+    TAN,
+    GREEN,
+    BLACK,
+    WHITE,
+    YELLOW,
+)
 from tiktoktoe.game import Game
 
 
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("tiktoktoe 2.0")
-pygame.display.init()
+pygame.font.init()
+WINNER_FONT = pygame.font.SysFont("comicsans", 100)
 
 
 def get_row_col_from_mouse(pos):
@@ -24,19 +37,39 @@ def main():
         clock.tick(FPS)
 
         if game.winner() != 0:
-            if game.winner() == str(WHITE):
-                print('WHITE Thang')
-            else:print('Black thang')
-            run = False
+            text = game.winner()
+            draw_text = WINNER_FONT.render(text, 1, YELLOW)
+            WIN.blit(
+                draw_text,
+                (
+                    WIDTH / 2 - draw_text.get_width() / 2,
+                    HEIGHT / 2 - draw_text.get_height() / 2,
+                ),
+            )
+            pygame.display.update()
+            # pygame.time.delay(3000)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
-                game.click(row,col)
-                print(game.get(row,col))
+                if event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    row, col = get_row_col_from_mouse(pos)
+                    game.clickleft(row, col)
+
+                elif event.button == 3:
+                    pos = pygame.mouse.get_pos()
+                    row, col = get_row_col_from_mouse(pos)
+                    game.clickright(row, col)
+
+                elif event.button == 3 or event.button == 5:
+                    pos = pygame.mouse.get_pos()
+                    row, col = get_row_col_from_mouse(pos)
+                    game.clickmid(row, col)
+
+                print(game.turn_W, game.turn_B)
         game.update()
     pygame.quit()
 
--- tiktoktoe/game.py
+++ tiktoktoe/game.py
@@ -4,48 +4,78 @@ from .constant import WIDTH, HEIGHT, COLUMN, ROW, SIZE, TAN, GREEN, BLACK, WHITE
 from tiktoktoe.board import Board
 
 
-
 class Game:
-    def __init__(self,win):
+    def __init__(self, win):
         self._init()
         self.win = win
 
     def _init(self):
         self.board = Board()
         self.color = WHITE
-        self.turn_W = 3
-        self.turn_B = 3
-    
+        self.turn_W = [3, 3, 3]
+        self.turn_B = [3, 3, 3]
+
     def update(self):
         self.board.draw(self.win)
         pygame.display.update()
-    
+
     def winner(self):
-        return self.board.winner() 
-    
+        text = self.board.winner()
+        if text != 0:
+            return text
+        else:
+            return 0
     def reset(self):
         self._init()
 
-    def winner(self):
-        return self.board.winner()
-        
-    def get(self,row,col):
-        return self.board.get(row,col)
+    def get(self, row, col):
+        text = self.board.get(row, col)
+        if text != 0:
+            return text
+        else:
+            return 0
 
-    def click(self,row,col):
-        if  self.turn_W > 0 and self.color==WHITE:
-            self.board.create_ox(row,col,self.color)
-            self.turn_W -= 1
-            self.chang_turn()
-            return True
-        if  self.turn_B > 0 and self.color==BLACK:
-            self.board.create_ox(row,col,self.color)
-            self.turn_B -= 1
-            self.chang_turn()
-            return True
+    def clickleft(self, row, col):
+        if self.turn_W[0] > 0 and self.color == WHITE:
+            if self.board.create_ox(row, col, self.color):
+                self.turn_W[0] = self.turn_W[0] - 1
+                self.chang_turn()
+                return True
+        if self.turn_B[0] > 0 and self.color == BLACK:
+            if self.board.create_ox(row, col, self.color):
+                self.turn_B[0] -= 1
+                self.chang_turn()
+                return True
         return False
+
+    def clickmid(self, row, col):
+        if self.turn_W[1] > 0 and self.color == WHITE:
+            if self.board.create_queen(row, col, self.color):
+                self.turn_W[1] -= 1
+                self.chang_turn()
+                return True
+        if self.turn_B[1] > 0 and self.color == BLACK:
+            if self.board.create_queen(row, col, self.color):
+                self.turn_B[1] -= 1
+                self.chang_turn()
+                return True
+        return False
+
+    def clickright(self, row, col):
+        if self.turn_W[2] > 0 and self.color == WHITE:
+            if self.board.create_king(row, col, self.color):
+                self.turn_W[2] -= 1
+                self.chang_turn()
+                return True
+        if self.turn_B[2] > 0 and self.color == BLACK:
+            if self.board.create_king(row, col, self.color):
+                self.turn_B[2] -= 1
+                self.chang_turn()
+                return True
+        return False
+
     def chang_turn(self):
         if self.color == WHITE:
             self.color = BLACK
-        else: self.color = WHITE
-
+        else:
+            self.color = WHITE
